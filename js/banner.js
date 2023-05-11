8dca6787c5ab7e432666a216d383368986b14e73
oRonold <or040918@gmail.com>
2023-05-10 21:09:07 -0300

    exercicios javascript

---
+++ js/banner.js
@@ -0,0 +1,19 @@
+//const mudaCor = ()=>{
+//
+//    let r = Math.round(Math.random()*255);
+//    let g = Math.round(Math.random()*255);
+//    let b = Math.round(Math.random()*255);
+//
+//    const elCabecalho = document.querySelector(".cabecalho");
+//    
+//    elCabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+//
+//    tmp = setTimeout(mudaCor, 1000);
+//}
+//
+//mudaCor();
+
+function mudaBanner(){
+
+    const img1 = document.querySelector(".banner-1 img")
+}
